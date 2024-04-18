585f0d412fa5e2a2969a94684e07caa983310143
henrique-amorim-one <164920630+henrique-amorim-one@users.noreply.github.com>
2024-04-18 16:44:06 -0300

    feat - começa projeto com backend

---
+++ crud/frontend/src/components/template/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Main.css"
+import Header from "./Header"
+
+export default props => 
+    <React.Fragment>
+        <Header/>
+        <main className="content">
+            Conteudo
+        </main>
+    </React.Fragment>
